7fc7630f641a10ba62561b5f71c03c248a8c0cac
catdiprinting <122374287+catdiprinting@users.noreply.github.com>
2025-12-29 09:37:11 -0600

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,6 @@
-# db.py
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./local.db")
 
@@ -16,5 +15,12 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

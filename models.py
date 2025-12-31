938abbe0b73d895c3d85da7f508de3d60a9f21c6
catdiprinting <122374287+catdiprinting@users.noreply.github.com>
2025-12-31 07:58:21 -0600

    Update models.py

--- models.py
+++ models.py
@@ -2,17 +2,11 @@
 from sqlalchemy import Column, Integer, String, Text
 from db import Base
 
-class Category(Base):
-    __tablename__ = "categories"
-    id = Column(Integer, primary_key=True, index=True)
-    category_uuid = Column(String(64), unique=True, index=True, nullable=False)
-    category_name = Column(String(255), nullable=False)
-    category_description = Column(Text, nullable=True)
 
-class Product(Base):
-    __tablename__ = "products"
+class CatalogItem(Base):
+    __tablename__ = "catalog_items"
+
     id = Column(Integer, primary_key=True, index=True)
     product_uuid = Column(String(64), unique=True, index=True, nullable=False)
     product_code = Column(String(128), nullable=True)
-    product_description = Column(Text, nullable=True)
-    category_uuid = Column(String(64), index=True, nullable=True)
+    description = Column(Text, nullable=True)

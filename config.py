fdbfb2f139bc2bc4d71d40377e11cc230e5e7268
catdiprinting <122374287+catdiprinting@users.noreply.github.com>
2025-12-31 08:12:56 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
+# config.py
 import os
 
 SERVICE_NAME = os.getenv("SERVICE_NAME", "catdi-4over-connector")
 PHASE = os.getenv("PHASE", "0.9")
 BUILD = os.getenv("BUILD", "ROOT_MAIN_PY_V2")
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./local.db")
-
-FOUR_OVER_BASE_URL = os.getenv("FOUR_OVER_BASE_URL", "https://api.4over.com").rstrip("/")
 FOUR_OVER_APIKEY = os.getenv("FOUR_OVER_APIKEY", "")
 FOUR_OVER_PRIVATE_KEY = os.getenv("FOUR_OVER_PRIVATE_KEY", "")
+FOUR_OVER_BASE_URL = os.getenv("FOUR_OVER_BASE_URL", "https://api.4over.com").rstrip("/")
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./local.db")

4aa2e247ebc4e4baf3cec0b71fed2242d42e52da
catdiprinting <122374287+catdiprinting@users.noreply.github.com>
2025-12-31 08:07:05 -0600

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,3 @@
-# models.py
-from sqlalchemy import Column, Integer, String, Text
-from db import Base
+from sqlalchemy.orm import declarative_base
 
-
-class CatalogItem(Base):
-    __tablename__ = "catalog_items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    product_uuid = Column(String(64), unique=True, index=True, nullable=False)
-    product_code = Column(String(128), nullable=True)
-    description = Column(Text, nullable=True)
+Base = declarative_base()

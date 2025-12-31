a464d39e361302a038cb601acd85b9dc5eed1b96
catdiprinting <122374287+catdiprinting@users.noreply.github.com>
2025-12-31 07:57:47 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 # config.py
 import os
+from pydantic import BaseModel
 
-FOUR_OVER_BASE_URL = os.getenv("FOUR_OVER_BASE_URL", "https://api.4over.com").rstrip("/")
-FOUR_OVER_APIKEY = os.getenv("FOUR_OVER_APIKEY", "")
-FOUR_OVER_PRIVATE_KEY = os.getenv("FOUR_OVER_PRIVATE_KEY", "")
 
-APP_PHASE = os.getenv("APP_PHASE", "0.6")
-APP_BUILD = os.getenv("APP_BUILD", "4over-ping-enabled")
-SERVICE_NAME = os.getenv("SERVICE_NAME", "catdi-4over-connector")
+class Settings(BaseModel):
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./local.db")
+
+    FOUR_OVER_BASE_URL: str = os.getenv("FOUR_OVER_BASE_URL", "https://api.4over.com")
+    FOUR_OVER_APIKEY: str = os.getenv("FOUR_OVER_APIKEY", "")
+    FOUR_OVER_PRIVATE_KEY: str = os.getenv("FOUR_OVER_PRIVATE_KEY", "")
+
+    SERVICE_NAME: str = os.getenv("SERVICE_NAME", "catdi-4over-connector")
+    PHASE: str = os.getenv("PHASE", "0.6")
+    BUILD: str = os.getenv("BUILD", "root-layout-db-ping-fourover-v1")
+
+
+settings = Settings()

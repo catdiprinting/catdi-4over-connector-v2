7625cfb30670f19d78e1eacca3e7acd07ce8d3a3
catdiprinting <122374287+catdiprinting@users.noreply.github.com>
2026-01-01 13:37:08 -0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-FOUR_OVER_BASE_URL = os.getenv("FOUR_OVER_BASE_URL", "https://api.4over.com").rstrip("/")
-FOUR_OVER_APIKEY = os.getenv("FOUR_OVER_APIKEY", "").strip()
-FOUR_OVER_PRIVATE_KEY = os.getenv("FOUR_OVER_PRIVATE_KEY", "").strip()
-
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./local.db").strip()
